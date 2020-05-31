bf219a89c95fec270847b9498440d115da89006c
MohammedSallam <mosallam06@gmail.com>
2020-05-31 15:27:43 +0200

    Last update

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 
 $conn = @mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$char = mysqli_set_charset($conn, 'utf8');
+$char = @mysqli_set_charset($conn, 'utf8');
 
 if($conn === false){
     die("<b>The target machine not responding...</b>" );
